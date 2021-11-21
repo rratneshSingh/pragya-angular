76acd0788d02665444551a65a7f85b25f8cf5640
ratnesh singh <dev.ratnesh.singh@gmail.com>
2021-11-21 21:09:30 +0530

    directives

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -14,6 +14,15 @@ export class AppComponent {
   green = 156;
   color = null;
 
+  active = false;
+  italic = false;
+  urls = [ 
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3ZgKry_t5w4ijJS3RM0FZ46fqKFf66SlNWg&usqp=CAU',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-_wX2kxYjnUEr5P-qk6c1oJN_nxYHKY1Skw&usqp=CAU'
+  ];
+
+ url = this.urls[0];
+
   changeColor() {
     var h1 = document.getElementById('myHeading');
     h1.style.color = 'red';
